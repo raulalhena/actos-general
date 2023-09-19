171f0b8371fce2b01c14014ee7b4f7bb2972e5d8
Raul <raul.alhena@gmail.com>
2023-09-20 01:40:29 +0200

    /server/upload folder ignored

--- client/src/components/ImageUploader/ImageUploader.tsx
+++ client/src/components/ImageUploader/ImageUploader.tsx
@@ -10,25 +10,6 @@ interface ImageUploaderProps {
 
 export const ImageUploader = ({ previewURL, imgVisibility, removeImage, onDrop, onDragOver }: ImageUploaderProps) => {
 
-    // const handleSubmit = (e: SubmitEvent) => {
-    //     e.preventDefault();
-    //     const url = 'http://localhost:5000/uploadFile';
-    //     const formData = new FormData();
-    //     formData.append('file', file);
-    //     formData.append('fileName', file.name);
-    //     const config = {
-    //         headers: {
-    //             'content-type': 'multipart/form-data',
-    //         },
-    //     };
-    //     console.log(file);
-    //     // Envío de fichero de imagen al backend para almacenarlo
-    //     // const resp = await fetch('http://localhost:5000/api/events, method: 'POST')
-    //     // const data = await resp.json();
-    //     // setPreviewURL(data.fileUrl);
-
-    // };
-
     return (   
         <div style={{ width: '300px' }} className={'drag-drop-zone image-uploader'} onDrop={onDrop} onDragOver={onDragOver}>
             <p>Drag files here to upload<img src={previewURL} style={{ objectFit: 'cover', display: imgVisibility }} alt='uploaded' width='300' height='300' /></p>
