f799003a5e252240f83eaee46092e84ca331736c
andreubltzr <andreubltzr11@gmail.com>
2023-09-19 15:03:01 +0200

    added event POST endpoint: create new event

--- server/src/events/dto/create-event.dto.ts
+++ server/src/events/dto/create-event.dto.ts
@@ -1,1 +1,38 @@
-export class CreateEventDto {}
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateEventDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsDate()
+    date: Date;
+
+    tags: string[];
+    category: string;
+    adress: string;
+    startTime: Date;
+    endTime: Date;
+    timeZone: string;
+    showStartTime: boolean;
+    showEndTime: boolean;
+    confirmed: boolean;
+    type: string;
+    mode: string;
+    image: string;
+    video: string;
+    capacity: number;
+    price: number;
+    payment: string;
+    contact: string;
+    language: [ string ];
+    web: string;
+    visibility: boolean;
+    status: boolean;
+    customForm: string;
+    form: Object
+}
--- server/src/events/schemas/event.schema.ts
+++ server/src/events/schemas/event.schema.ts
@@ -4,7 +4,7 @@ import { User } from 'src/users/entities/user.entity';
 
 export type EventDocument = HydratedDocument<Event>;
 
-Schema({timestamps: true});
+@Schema({timestamps: true})
 export class Event {
   @Prop({ required: [true, 'El nombre del evento es requerido.'] })
   name: string;
@@ -13,7 +13,7 @@ export class Event {
   description: string;
 
   @Prop()
-  tags: string;
+  tags: string[];
 
   @Prop()
   category: string;
@@ -54,14 +54,14 @@ export class Event {
   @Prop()
   video: string;
 
-  @Prop()
-  qr: string;
+  @Prop({default: []})
+  qr: string[];
 
-  @Prop({type: mongoose.Schema.Types.ObjectId, ref: User.name})
-  attendees: ObjectId[];
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: User.name }], default: [] })
+  attendees: mongoose.Types.ObjectId[];
 
-  @Prop({type: mongoose.Schema.Types.ObjectId, ref: User.name})
-  submitted: ObjectId[];
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: User.name }], default: [] })
+  submitted: mongoose.Types.ObjectId[];
 
   @Prop()
   capacity: number;
@@ -81,16 +81,16 @@ export class Event {
   @Prop()
   web: string;
 
-  @Prop()
+  @Prop({default: false})
   visibility: boolean;
 
-  @Prop()
+  @Prop({default: false})
   status: boolean;
 
   @Prop()
   customForm: string;
 
-  @Prop()
+  @Prop({type: Object})
   form: Object;
 }
 
