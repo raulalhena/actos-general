a5c1d4beb60656c9af3a653de8e335e50d3eb209
andreubltzr <andreubltzr11@gmail.com>
2023-09-19 12:00:14 +0200

    added .gitignore

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(8000);
 }
 bootstrap();
