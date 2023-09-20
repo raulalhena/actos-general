7f0396271a723459cc9fb225b94a1ee4053f8d2b
Raul <raul.alhena@gmail.com>
2023-09-20 02:10:48 +0200

    solving ImageUploader problems from applying styles

--- client/src/components/ImageUploader/ImageUploader.tsx
+++ client/src/components/ImageUploader/ImageUploader.tsx
@@ -11,22 +11,34 @@ interface ImageUploaderProps {
 
 export const ImageUploader = ({ previewURL, imgVisibility, removeImage, onDrop, onDragOver }: ImageUploaderProps) => {
 
-    return (
-        <div className={styles.uploadImgContainer}>
-            <label className={styles.label} >
-                Imagen
-            </label>
-            <br />
-            <div className={styles.dragDropZone}
-                onDrop={onDrop}
-                onDragOver={onDragOver}
-            >
-                <PiUploadSimpleLight className={styles.icon}/>
-                <p>Arrastra una imagen.<img src={previewURL} style={{ objectFit: 'cover', display: imgVisibility }} alt='uploaded' width='300' height='300' /></p>
-                
-            </div>
+    // return (
+    // <div className={styles.uploadImgContainer}>
+    //     <label className={styles.label} >
+    //         Imagen
+    //     </label>
+    //     <br />
+    // <div className={styles.dragDropZone} onDrop={onDrop} onDragOver={onDragOver}>
+    //     <PiUploadSimpleLight className={styles.icon}/>
+    //     <p>Arrastra una imagen.<img src={previewURL} style={{ objectFit: 'cover', display: imgVisibility }} alt='uploaded' width='300' height='300' /></p>
+    //     <button onClick={removeImage}>Remove</button>
+    // </div>
+           
+    // </div>
+    // );
+    return (   
+        <div style={{ width: '300px' }} className={'drag-drop-zone image-uploader'} onDrop={onDrop} onDragOver={onDragOver}>
+            <p>Drag files here to upload<img src={previewURL} style={{ objectFit: 'cover', display: imgVisibility }} alt='uploaded' width='300' height='300' /></p>
             <button onClick={removeImage}>Remove</button>
         </div>
-        
     );
 };
+
+// export const ImageUploader = ({ previewURL, imgVisibility, removeImage, onDrop, onDragOver }: ImageUploaderProps) => {
+
+//     return (   
+//         <div style={{ width: '300px' }} className={'drag-drop-zone image-uploader'} onDrop={onDrop} onDragOver={onDragOver}>
+//         </div>
+//             <p>Drag files here to upload<img src={previewURL} style={{ objectFit: 'cover', display: imgVisibility }} alt='uploaded' width='300' height='300' /></p>
+//             <button onClick={removeImage}>Remove</button>
+//     );
+// };
