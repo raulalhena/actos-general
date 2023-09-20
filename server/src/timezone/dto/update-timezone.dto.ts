df4ec1513114400b54aea30c204b8e04f4f0e066
Raul <raul.alhena@gmail.com>
2023-09-20 12:04:02 +0200

    deleted entities from nest

--- server/src/timezone/dto/update-timezone.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTimezoneDto } from './create-timezone.dto';
-
-export class UpdateTimezoneDto extends PartialType(CreateTimezoneDto) {}
--- server/src/timezone/timezone.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TimezoneService } from './timezone.service';
-import { TimezoneController } from './timezone.controller';
-
-@Module({
-  controllers: [TimezoneController],
-  providers: [TimezoneService]
-})
-export class TimezoneModule {}
