3e542d99babf1a795273118616b1a9b98c4a5f6d
Raul <raul.alhena@gmail.com>
2023-09-20 22:52:17 +0200

    fixed route errore in NavBar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from './Navbar.module.css';
+import styles from './NavBar.module.css';
 import Logo from '../../assets/logo.png';
 
 function Navbar() {
