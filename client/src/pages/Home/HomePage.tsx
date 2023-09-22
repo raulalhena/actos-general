465b8371d1dbff59107f2f3aa6ff8e5a7f6061a9
Teresa <teresamadridejosm@gmail.com>
2023-09-22 11:57:36 +0200

    structure HomePage

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -1,5 +1,7 @@
 import HomePageHeader from '../../components/HomePageHeader/HomePageHeader';
 import CardEvent from '../../components/CardEvent/CardEvent';
+import styles from './page.module.css';
+
 const HomePage = () => {
     const eventData = {
         title: 'Evento de Ejemplo',
@@ -14,7 +16,20 @@ const HomePage = () => {
         <>
             <HomePageHeader />
             <button >Crear nuevo evento</button>
+            <section className={styles.section}>
+            <div className={styles.title}>
+                <h1 className={styles.dash}>—</h1>
+                <h1>Categorías</h1>
+            </div>
+            </section>
+            <section className={styles.section}>
+            <div className={styles.title}>
+                <h1 className={styles.dash}>—</h1>
+                <h1>Eventos destacados</h1>
+            </div>
             <CardEvent eventData={eventData} eventId={0} />
+            </section>
+
         </>
     );
 };
