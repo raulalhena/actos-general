4f76c26ff58b19d5ee0e445adf5cae62d32aae8a
Raul <raul.alhena@gmail.com>
2023-09-24 00:13:03 +0200

    qr created and store the url to the end point: attendance with event id as parameter

--- server/src/utils/qr.generator.ts
+++ server/src/utils/qr.generator.ts
@@ -4,15 +4,13 @@ import { createWriteStream } from 'fs';
 
 const generateEventQR = async (eventId: Types.ObjectId) => {
     try{
-        const qr_svg = qr.image(`${eventId}_qr`, { type: 'svg' });
-        qr_svg.pipe(createWriteStream(`qr_events/${eventId}.svg`));   
-        const eventQR = qr.imageSync(eventId.toString(), { type: 'svg' });
-
+        const qr_svg = qr.image(eventId.toString(), { type: 'svg' });
+        qr_svg.pipe(createWriteStream(`qr_events/${eventId.toString()}.svg`));   
+        const eventQR = qr.imageSync(`http://localhost:8000/api/events/attendance/${eventId.toString()}`, { type: 'svg' });
         return eventQR;
     } catch(error){ 
         console.log(error);
     }
-    
 }
 
 const generateUserQR = async (eventId: mongoose.Schema.Types.ObjectId, userId: mongoose.Schema.Types.ObjectId) => {
