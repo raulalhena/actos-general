d4495d53d123ec0444dedd4230c2ab09740bfc83
Raul <raul.alhena@gmail.com>
2023-09-27 00:42:23 +0200

    installed swagger, configured swagger in [server_url]/docs. Working

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	app.enableCors();
+	const config = new DocumentBuilder().setTitle('Aplicación ACTOS')
+						.setDescription('Aplicación de gestión de eventos')
+						.setVersion('v1')
+						.addTag('events')
+						.build();
 
+	const document = SwaggerModule.createDocument(app, config);						
+	SwaggerModule.setup('docs', app, document);
+	
+	app.enableCors();
 	app.setGlobalPrefix('api');
 
 	await app.listen(8000);
