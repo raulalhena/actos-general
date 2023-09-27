1bde2b4bb525a67be9fcbb776f37222b54e2db81
Raul <raul.alhena@gmail.com>
2023-09-27 14:15:46 +0200

    try fix initial context issue

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,5 +1,4 @@
 import { createContext } from 'react';
-import { User } from '../interfaces/User';
 import { AuthContextInterface } from '../interfaces/authContext';
   
 const initialContext = {
--- client/src/interfaces/textInputProps.ts
+++ client/src/interfaces/textInputProps.ts
@@ -1,5 +1,3 @@
-import { ChangeEvent } from 'react';
-
 export interface TextInputProps {
     label: string;
     subtitle?: string;
