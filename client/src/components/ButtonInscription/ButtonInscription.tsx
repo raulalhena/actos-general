7447939cffb13f122a80f5e1557e4f675a75c390
Teresa <teresamadridejosm@gmail.com>
2023-09-28 11:06:15 +0200

    Button inscription

---
+++ client/src/components/ButtonInscription/ButtonInscription.tsx
@@ -0,0 +1,17 @@
+import { ButtonSubmitProps } from '../../interfaces/buttonSubmitProps';
+import styles from './ButtonInscription.module.css';
+import { IoTicketOutline } from 'react-icons/io5';
+
+const ButtonInscription = (props: ButtonSubmitProps) => {
+    const { label, onClick } = props;
+    return (
+        <div className={styles.container}>
+            <button className={styles.button} onClick={onClick}>
+                <IoTicketOutline className={styles.ticketIcon} />
+                {label}
+            </button>
+        </div>
+    );
+};
+
+export default ButtonInscription;
