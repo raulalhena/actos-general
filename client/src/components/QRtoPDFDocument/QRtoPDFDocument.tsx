6b5cde8ee439786b95b086f76ab45b2ae33587d3
Teresa <teresamadridejosm@gmail.com>
2023-09-28 10:08:34 +0200

    .

--- client/src/components/QRtoPDFDocument/QRtoPDFDocument.tsx
+++ client/src/components/QRtoPDFDocument/QRtoPDFDocument.tsx
@@ -1,7 +1,6 @@
 import { Document, Image, Page, View } from '@react-pdf/renderer';
 import Img from '../../../../server/qr_events/651441b618f217f1a9d762ea.png';
 
-
 type Props = {
     qrImg: string;
     eventName: string;
