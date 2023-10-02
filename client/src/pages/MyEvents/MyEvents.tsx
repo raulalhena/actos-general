c2a713e3b7bb5f1c395d3948c0f676211a42a4c8
Raul <raul.alhena@gmail.com>
2023-10-03 00:46:24 +0200

    created MyEvents page and added protected route for it

--- client/src/pages/MyEvents/MyEvents.tsx
+++ client/src/pages/MyEvents/MyEvents.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import CardEvent from '../../components/CardEvent/CardEvent';
 
 const MyEvents = () => {
     return (
-        <div>MyEvents</div>
+        <section className={styles.section}>
+            <div className={styles.title}>
+                <h1 className={styles.dash}>—</h1>
+                <h1>Mis Eventos</h1>
+            </div>
+
+            <div className={styles.cardGrid}>
+                {latestEvents.map((event, index) => (
+                    <CardEvent key={index} eventData={event} />
+                ))}
+            </div>
+        </section>
     );
 };
 
