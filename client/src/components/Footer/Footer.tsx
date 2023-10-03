96acb462695e632266fc646f3a2373190bc7a6dd
Teresa <teresamadridejosm@gmail.com>
2023-10-03 16:18:39 +0200

    Scroll button, footer and fag page styled

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -18,74 +18,80 @@ const socialMediaLinks = [
 const Footer = () => {
     return (
         <>
-            <div className={styles.footer}>
-                <section className={styles.sectionFooter}>
-                    <div className={styles.brandSection}>
-                        <img src={logo} className={styles.logo} alt="Logo" />
-                        <p className={styles.brandName}>Fundación Privada Nous Cims</p>
-                    </div>
-                </section>
-                <section className={styles.sectionFooter}>
-                    <div className={styles.contactSection}>
-                        <h1 className={styles.titleFooter}>Contacto</h1>
-                        <h2 className={styles.contactInfo}>
-              Oficina Nous Cims en Barcelona
-                        </h2>
-                        <p className={styles.contactAddress}>
-              Entença, 332-334. 7ª planta
-                            <br />
-              08029 Barcelona
-                            <br />
-              Tel: +34 677920250
-                        </p>
-                    </div>
-                </section>
-                <section className={styles.sectionFooter}>
-                    <div className={styles.socialSection}>
-                        <h1 className={styles.titleFooter}>#nouscims</h1>
-                        <div className={styles.socialIcons}>
-                            {socialMediaLinks.map((link, index) => (
-                                <div className={styles.iconModeContainer} key={index}>
-                                    <a href={link.url} className={styles.iconLink} target="_blank" >
-                                        <div className={styles.iconContainer}>{link.icon}</div>
-                                    </a>
-                                    <span className={styles.mode}></span>
-                                </div>
-                            ))}
+            <div className={styles.container}>
+                <div className={styles.footer}>
+                    <section className={styles.sectionFooter}>
+                        <div className={styles.brandSection}>
+                            <img src={logo} className={styles.logo} alt="Logo" />
+                            <p className={styles.brandName}>Fundación Privada Nous Cims</p>
                         </div>
-                    </div>
-                </section>
-            </div>
-            <div className={styles.copyright}>
-        © 2023 Nous Cims by ACTOS. Todos los derechos
-        reservados&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                <a
-                    className={styles.copyrightLink}
-                    href="https://www.nouscims.com/aviso-legal/"
-                >
-          Aviso Legal
-                </a>
-        &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                <a
-                    className={styles.copyrightLink}
-                    href="https://www.nouscims.com/politica-privacidad/"
-                >
-          Política de Privacidad
-                </a>
-        &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                <a
-                    className={styles.copyrightLink}
-                    href="https://www.nouscims.com/politica-de-cookies/"
-                >
-          Política de Cookies
-                </a>
-        &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
-                <a
-                    className={styles.copyrightLink}
-                    href="https://www.nouscims.com/politica-de-calidad/"
-                >
-          Política de Calidad
-                </a>
+                    </section>
+                    <section className={styles.sectionFooter}>
+                        <div className={styles.contactSection}>
+                            <h1 className={styles.titleFooter}>Contacto</h1>
+                            <h2 className={styles.contactInfo}>
+                Oficina Nous Cims en Barcelona
+                            </h2>
+                            <p className={styles.contactAddress}>
+                Entença, 332-334. 7ª planta
+                                <br />
+                08029 Barcelona
+                                <br />
+                Tel: +34 677920250
+                            </p>
+                        </div>
+                    </section>
+                    <section className={styles.sectionFooter}>
+                        <div className={styles.socialSection}>
+                            <h1 className={styles.titleFooter}>#nouscims</h1>
+                            <div className={styles.socialIcons}>
+                                {socialMediaLinks.map((link, index) => (
+                                    <div className={styles.iconModeContainer} key={index}>
+                                        <a
+                                            href={link.url}
+                                            className={styles.iconLink}
+                                            target="_blank"
+                                        >
+                                            <div className={styles.iconContainer}>{link.icon}</div>
+                                        </a>
+                                        <span className={styles.mode}></span>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div className={styles.copyright}>
+          © 2023 Nous Cims by ACTOS. Todos los derechos
+          reservados&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a
+                        className={styles.copyrightLink}
+                        href="https://www.nouscims.com/aviso-legal/"
+                    >
+            Aviso Legal
+                    </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a
+                        className={styles.copyrightLink}
+                        href="https://www.nouscims.com/politica-privacidad/"
+                    >
+            Política de Privacidad
+                    </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a
+                        className={styles.copyrightLink}
+                        href="https://www.nouscims.com/politica-de-cookies/"
+                    >
+            Política de Cookies
+                    </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a
+                        className={styles.copyrightLink}
+                        href="https://www.nouscims.com/politica-de-calidad/"
+                    >
+            Política de Calidad
+                    </a>
+                </div>
             </div>
         </>
     );
