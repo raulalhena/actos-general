70cf86b625530cd5a1180b50314b6feb765d4ccd
Yumi-Namie <rn.yumi@gmail.com>
2023-10-04 19:29:10 +0200

    search bar - general filter working

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './SearchBar.module.css';
 import { FaSearch } from 'react-icons/fa';
 
 function SearchBar() {
+    const [ searchValue, setSearchValue ] = useState('');
+    const navigate = useNavigate();
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value);
+    };    
+
+    const handleSearchClick = () => {
+        navigate(`/allevents?keywords=${searchValue}`);
+    };
+
     return (
         <div className={styles.searchBar}>
+            <div></div>
             <input
                 type="text"
                 className={styles.searchInput}
                 placeholder="Type to search"
+                value={searchValue}
+                onChange={handleInputChange}
             />
-            <button className={styles.searchIcon}>
+            <button className={styles.searchIcon} onClick={handleSearchClick}>
                 <FaSearch />
             </button>
         </div>
--- client/src/pages/AllEvents/AllEvents.tsx
+++ client/src/pages/AllEvents/AllEvents.tsx
@@ -3,22 +3,52 @@ import CardEvent from '../../components/CardEvent/CardEvent';
 import styles from './AllEvents.module.css';
 import { CardEventProps } from '../../interfaces/cardEventProps';
 import Preloader from '../../components/Preloader/Preloader';
+import { useLocation } from 'react-router-dom';
 
 const AllEvents = () => {
     const [ eventData, setEventData ] = useState<CardEventProps['eventData'][]>([]);
     const [ isLoading, setIsLoading ] = useState(true);
+    const location = useLocation();
 
     useEffect(() => {
-        fetch('http://localhost:8000/api/events')
+        console.log('Current Location:', location);
+        const searchParams = new URLSearchParams(location.search);
+        const keywords = searchParams.get('keywords');
+        console.log('Keywords:', keywords);
+
+        // Modify the API endpoint to include the search query
+        const apiUrl = keywords
+            ? `http://localhost:8000/api/events/search?filters=name,category,tags,mode,language&keywords=${keywords}`
+            : 'http://localhost:8000/api/events';
+
+        console.log('API URL:', apiUrl);
+
+        fetch(apiUrl)
             .then((response) => response.json())
-            .then((data) => {
-                setEventData(data);
-                setIsLoading(false);
-            })
+            .then((res) => {
+                if (Array.isArray(res.data)) {
+                    setEventData(res.data);
+                    setIsLoading(false);
+                } else {
+                    console.error('API response is not an array:', res);
+                }
+            })            
             .catch((error) => {
                 console.error('Error al obtener datos:', error);
             });
-    }, []);
+    }, [ location.search ]);
+
+    // useEffect(() => {
+    //     fetch('http://localhost:8000/api/events')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setEventData(data);
+    //             setIsLoading(false);
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error al obtener datos:', error);
+    //         });
+    // }, []);
 
     return (
         <>
