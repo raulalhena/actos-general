2a3acb5ce222c8a004cf1b6045f5d8c55037b243
andreubltzr <andreubltzr11@gmail.com>
2023-10-05 20:51:49 +0200

    ProtectedRoute: reloading page redirects to /login fixed

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
-import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import { ReactNode } from 'react';
 
 interface Props {
     children: ReactNode;
@@ -9,7 +9,7 @@ interface Props {
 export const ProtectedRoute = ( { role, children }: Props ) => {
     const { isLogged, user } = useAuth();
 
-    return (isLogged && role.includes(user.role)) ? children : <Navigate to='/login' />;
+    return (isLogged && role.includes(user.role)) ? children : null;
 };
 
 export default ProtectedRoute;
